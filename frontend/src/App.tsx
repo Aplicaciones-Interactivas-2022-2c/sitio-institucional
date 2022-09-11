0c596b0f6a4cb42bb1dec26773060f4ea2f75a69
Alfonso Campodonico <acampodonico@heroku.com>
2022-09-11 10:50:07 -0300

    navbar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+import NavBar from "./components/NavBar"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+
+          <NavBar />
+          
+  </ChakraProvider>
+)
