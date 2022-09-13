00ded006017777343e4b3fd0f848f4b814af52bd
TadeoCullen <tadeo.cullen@mercadolibre.com>
2022-09-12 23:59:07 -0300

    Utilizacion de ChakraUi y ChocUi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ChakraProvider,
+  Container,
+  theme,
+} from '@chakra-ui/react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import AboutPage from 'pages/AboutPage';
+import HomePage from 'pages/HomePage';
+import NavBarChoc from 'components/NavBarChoc';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+        <NavBarChoc />
+        <Container bg='blue.600'  maxW='container.xl' centerContent>
+        <Router>
+        <Switch>
+        
+          <Route path="/about">
+            <AboutPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+      </Container>
+    </ChakraProvider>
   );
 }
 
---
+++ frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <Box p={10} h="100%">
+      <h2>ABOUT PAGE</h2>
+    </Box>
+  );
+};
+
+export default AboutPage;
