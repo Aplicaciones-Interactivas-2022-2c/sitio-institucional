b4bc5b075c90aabf6c1c809bda874b42dc4cdfcc
TadeoCullen <tadeo.cullen@mercadolibre.com>
2022-09-25 11:00:36 -0300

    redirecting when page not allowed (login and signin when allready logedin)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
-import { ChakraProvider, Container, theme } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import { SignInPage, SignUpPage, HomePage, AboutPage } from './pages';
 import Navbar from 'components/Navbar';
 import React from 'react';
-import { AuthContextProvider } from 'context/AuthContext';
+import { useAuthContext } from 'hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
-    <ChakraProvider theme={theme}>
-      <AuthContextProvider>
-        <Router>
-          <Navbar />
-          <Container bg="blue.600" maxW="container.xl" centerContent>
-            <Routes>
-              <Route path="/about" element={<AboutPage />}></Route>
-              <Route path="/home" element={<HomePage />}></Route>
-              <Route path="/signin" element={<SignInPage />}></Route>
-              <Route path="/signup" element={<SignUpPage />}></Route>
-            </Routes>
-          </Container>
-        </Router>
-      </AuthContextProvider>
-    </ChakraProvider>
+    <Router>
+      <Navbar />
+      <Container bg="blue.600" maxW="container.xl" centerContent>
+        <Routes>
+          <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route
+            path="/signin"
+            element={!user ? <SignInPage /> : <Navigate to="/home" />}
+          ></Route>
+          <Route
+            path="/signup"
+            element={!user ? <SignUpPage /> : <Navigate to="/home" />}
+          ></Route>
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
