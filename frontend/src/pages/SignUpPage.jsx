be17d75fc28d44c85fdc8171822e1ceed297721e
Alfonso Campodonico <acampodonico@heroku.com>
2022-09-25 12:36:41 -0300

    searcher

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,9 +2,10 @@ import { Box } from '@chakra-ui/react';
 import React from 'react';
 import { SignUp} from 'components/SignUp';
 import DropdownFrecuencia from 'components/DropdownFrecuencia';
+import Searcher from 'components/Searcher';
 
 export const SignUpPage = () => {
   return (
-    <DropdownFrecuencia></DropdownFrecuencia>
+    <Searcher></Searcher>
   );
 };
